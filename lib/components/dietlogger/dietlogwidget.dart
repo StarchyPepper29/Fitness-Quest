88fbe3c3d5b3a93ac1e712754d4a32232a2eaeef
Umais Usmani <98653230+StarchyPepper29@users.noreply.github.com>
2024-03-21 21:11:15 +0500

    Created Goals Screen

---
+++ lib/components/dietlogger/dietlogwidget.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import '../../services/fsda.dart';
+
+class DietLogger extends StatefulWidget {
+  const DietLogger({super.key});
+
+  @override
+  State<DietLogger> createState() => _DietLoggerState();
+}
+
+class _DietLoggerState extends State<DietLogger> {
+  @override
+  Widget build(BuildContext context) {
+    return Column();
+  }
+}
--- lib/components/goals/dietgoals.dart
+++ lib/components/goals/dietgoals.dart
@@ -1,17 +1,131 @@
 import 'package:flutter/material.dart';
+import '../dietlogger/dietlogwidget.dart';
 
 class Diet extends StatefulWidget {
-  const Diet({super.key});
+  const Diet({Key? key}) : super(key: key); // Fix the constructor
 
   @override
   State<Diet> createState() => _DietState();
 }
 
 class _DietState extends State<Diet> {
+  static const totalCalories = 2000;
+
   @override
   Widget build(BuildContext context) {
-    return (Container(
-      child: Text('Diet Goals'),
-    ));
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.all(20),
+            margin: const EdgeInsets.all(20),
+            color: Colors.blue,
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text(
+                      'Total Calories',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 24,
+                      ),
+                    ),
+                    Text(totalCalories.toString(),
+                        style:
+                            const TextStyle(color: Colors.white, fontSize: 24)),
+                  ],
+                ),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Protein (g)',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 24,
+                      ),
+                    ),
+                    Text(
+                      '0',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 24,
+                      ),
+                    ),
+                  ],
+                ),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Carbs (g)',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 24,
+                      ),
+                    ),
+                    Text(
+                      '0',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 24,
+                      ),
+                    ),
+                  ],
+                ),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Fat (g)',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 24,
+                      ),
+                    ),
+                    Text(
+                      '0',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 24,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Container(
+              padding: const EdgeInsets.all(20),
+              margin: const EdgeInsets.all(20),
+              color: Colors.red,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      // Add onPressed functionality
+                    },
+                    child: Text('Edit Goals'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          // Remove the extra argument
+                          return DietLogger();
+                        },
+                      );
+                    },
+                    child: Text('Log Today\'s Meals'),
+                  ),
+                ],
+              ))
+        ],
+      ),
+    );
   }
 }
